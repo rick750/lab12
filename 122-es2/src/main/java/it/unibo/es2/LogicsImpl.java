39458980478e84c0711e0cf0ceaf1c10fa02b516
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-12-09 10:47:01 +0100

    Added Logic interface and implementation

---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,43 @@
+package it.unibo.es2;
+
+import java.util.List;
+
+public class LogicsImpl implements Logics {
+
+	@Override
+	public List<Boolean> enablings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hit(int elem) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String result() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean toQuit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Integer> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
